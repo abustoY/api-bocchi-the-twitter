5d0d46b977bfb8fc980537163407e3194fe86f3f
akihiko <akihiko@akihikonoMacBook-Air.local>
2025-08-07 19:51:21 +0900

    いらないimportを削除

--- src/test/java/com/yotsuba/bocchi/TweetRepositoryTest.java
+++ src/test/java/com/yotsuba/bocchi/TweetRepositoryTest.java
@@ -5,8 +5,6 @@ import com.github.database.rider.junit5.api.DBRider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 
